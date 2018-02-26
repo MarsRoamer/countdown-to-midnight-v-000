50cc070d430365777a0e6bdf1c6568a739445e64
Jonathan Tiede <skysoldiermars@gmail.com>
2018-02-26 19:38:57 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -8,4 +8,11 @@ def countdown(num)
   "HAPPY NEW YEAR!"
 end
 
-countdown(15)
+def countdown_with_sleep(num)
+  while num > 0
+    puts "#{num} SECOND(S)!"
+    num -= 1
+    sleep(1)
+  end
+  "HAPPY NEW YEAR!"
+end
